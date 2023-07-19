055a5ab192db331131adeb43cef09cad5d35d369
username <email>
2023-07-19 10:43:42 -0700

    Make small refactor to Lexer.lex

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -17,16 +17,14 @@ let is_single = function
   | _ -> false
 
 let lex { source; column } =
-  let len = String.length source in
-
-  let column' = column + 1 in
-
   let make_one_of c cs len col =
     (One c, make_lexer (String.sub cs 1 len) col)
   in
 
+  let column' = column + 1 in
+
   match String.hd source with
-  | c when is_single c -> make_one_of c source len column'
+  | c when is_single c -> make_one_of c source (String.length source) column'
   | d when Char.is_digit d ->
       let num, rest = String.span (fun c -> not @@ is_single c) source in
       if String.length num = 1 then
